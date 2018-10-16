efa53d53cc585c838c62bedd82499df3b90e131b
VikaGolub <40851988+VikaGolub@users.noreply.github.com>
2018-10-16 15:24:48 +0300

    Update Will there be enough space.py

--- Will there be enough space.py
+++ Will there be enough space.py
@@ -2,9 +2,9 @@
 Codewars: Will there be enough space?
 '''
 def enough(cap, on, wait):
-    if wait + on - cap:
-        print("There are 10 free places in the bus!")
-        return 10
+    if wait + on > cap:
+        print("There are free places in the bus!")
+        return wait + on - cap
     else:
         print("No free places!")
         return 0
